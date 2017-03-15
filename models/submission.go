adc065e6afa6ee871c20e531acd9ed2b88c5692c
Tyson Willey <tyson.willey@flhosp.org>
2017-03-14 23:32:44 -0400

    Cleaned up tabs for spacing.

--- models/submission.go
+++ models/submission.go
@@ -1,12 +1,12 @@
 package models
 
 type Submission struct{
-	ID 						string	`json:"id"`
-	CollectionID 	string	`json:"collectionId"`
-	Title 				string	`json:"title"`
-	Status 				string	`json:"statusId"`
-	Data 					string	`json:"data"`
-	DateCreated 	int64		`json:"dateCreated"`
+	ID            string `json:"id"`
+	CollectionID  string `json:"collectionId"`
+	Title         string `json:"title"`
+	Status        string `json:"statusId"`
+	Data          string `json:"data"`
+	DateCreated   int64  `json:"dateCreated"`
 }
 
 func GetSubmission(id string) (*Submission, error) {
